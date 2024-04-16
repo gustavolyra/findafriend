f2424713a422e3d248488f4f18c1d5ebc1278d28
Gustavo Lyra <gustavo.c.lyra@live.com>
2024-04-16 09:53:19 -0300

    e2e search pet city, breed and age

--- src/http/controllers/pet/test/search.spec.ts
+++ src/http/controllers/pet/test/search.spec.ts
@@ -1,9 +1,8 @@
 import { app } from "@/app";
 import { prisma } from "@/lib/prisma";
 import { makeOrg } from "@/use-cases/test/factory/make-org.factory";
-import { randomUUID } from "crypto";
 import request from "supertest";
-import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { afterAll, afterEach, beforeAll, beforeEach, describe, expect, it } from "vitest";
 import { createAndAuthenticateUser } from "../../utils/test/create-and-authenticate";
 
 
@@ -16,7 +15,7 @@ describe('Search Pet (e2e)', () => {
     await app.close()
   })
 
-  it('should be able to search a pet by city', async () => {
+  it('should be able to search a pet by city age and breed', async () => {
     const { token } = await createAndAuthenticateUser(app)
     const city = 'city A'
     const org = await prisma.org.create({ data: makeOrg({ city }) })
@@ -29,7 +28,41 @@ describe('Search Pet (e2e)', () => {
       }
     })
     const response = await request(app.server).get('/pet').set('Authorization', `Bearer ${token}`)
-      .query({ city, minAge: 2, maxAge: 5, breed: 'poodle', orgId: org.id })
+      .query({ city, minAge: 2, maxAge: 5, breed: 'poodle' })
+
+    expect(response.statusCode).toEqual(201)
+  })
+  it('should be able to search a pet by city and age ', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+    const city = 'city A'
+    const org = await prisma.org.create({ data: makeOrg({ city }) })
+    await prisma.pet.create({
+      data: {
+        name: 'john2',
+        age: 5,
+        breed: 'poodle',
+        orgId: org.id,
+      }
+    })
+    const response = await request(app.server).get('/pet').set('Authorization', `Bearer ${token}`)
+      .query({ city, minAge: 2, maxAge: 5 })
+
+    expect(response.statusCode).toEqual(201)
+  })
+  it('should be able to search a pet by city', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+    const city = 'city B'
+    const org = await prisma.org.create({ data: makeOrg({ city }) })
+    await prisma.pet.create({
+      data: {
+        name: 'john',
+        age: 5,
+        breed: 'poodle',
+        orgId: org.id,
+      }
+    })
+    const response = await request(app.server).get('/pet').set('Authorization', `Bearer ${token}`)
+      .query({ city })
 
     expect(response.statusCode).toEqual(201)
   })
